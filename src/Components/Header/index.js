3b1390a56ac15df9761e6517e6fc3da3b782ca50
Austin Williams <austinwill264@gmail.com>
2022-01-13 10:29:56 -0800

    css for all devices

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,7 +5,7 @@ export default function Header() {
 
     return (
         <div className="header-container">
-            <h1>Nuvolum: Carousel Project</h1>
+            <h1>Nuvolum: Development Test</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eleifend sapien nunc, quis scelerisque dui dapibus non. Suspendisse potenti. In finibus tincidunt diam. Maecenas ex nisi, suscipit at risus at, cursus vestibulum nunc. Etiam enim metus, varius eget arcu at, sodales ultricies leo. Phasellus gravida dignissim nulla id ornare. Ut mollis turpis eu venenatis tempus. </p>
         </div>
     )
