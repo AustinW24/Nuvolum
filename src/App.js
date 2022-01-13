f0d2f76f3866991a64c6f448726fb13d7f1e1054
Austin Williams <austinwill264@gmail.com>
2022-01-12 17:18:38 -0800

    make it better

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Slider from './Components/Slider'
-
 
 function App() {
   return (
-    <div className="App">
-        <Router>
-                <Route path="/" component={Slider}/>
-        </Router>
-
-    </div>
+    <Slider />
   );
 }
 
